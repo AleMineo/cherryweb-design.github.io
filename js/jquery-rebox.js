5706720df354ac23dc768e318253e7ca3718a322
alessandra mineo <alessandra.mineo@tiscali.it>
2023-08-07 16:28:49 +0200

    Add files via upload

--- js/jquery-rebox.js
+++ js/jquery-rebox.js
@@ -21,7 +21,7 @@
 		selector: null,        // the selector to delegate to, should be to the <a> which contains an <img>
 		prev: '&laquo;',        // use an image, text, whatever for the previous button
 		next: '&raquo;',        // use an image, text, whatever for the next button
-		loading: '@',          // use an image, text, whatever for the loading notification
+		loading: 'º',          // use an image, text, whatever for the loading notification
 		close: '&times;',      // use an image, text, whatever for the close button
 		speed: 400,            // speed to fade in or out
 		zIndex: 1000,          // zIndex to apply to the outer container
