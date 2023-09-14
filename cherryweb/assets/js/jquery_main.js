7610e09706cd6782b12cac7acb43c5dd85a417bd
alessandra mineo <alessandra.mineo@tiscali.it>
2023-09-14 13:58:30 +0200

    Add files via upload

--- cherryweb/assets/js/jquery_main.js
+++ cherryweb/assets/js/jquery_main.js
@@ -33,20 +33,76 @@ $(window).scroll(function () {
 // external js: isotope.pkgd.js
 
 $(document).ready(function() {
+  var isMobile =  /Android|webOS|iPhone|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)
 
-  // init Isotope
-  var $container = $('.isotope').isotope({
+  var $portfolioContainer = $('#portfolio .isotope').isotope({
     itemSelector: '.element',
     layoutMode: 'fitRows',
     getSortData: {
       category: '[data-category]'
     }
+  })
+
+  var $hobbiesContainer = $('#hobbies .isotope').isotope({
+    itemSelector: '.element',
+    layoutMode: 'fitRows'
+  })
+
+  var initShow = isMobile ? 2 : 6; //number of items loaded on init & onclick load more button
+  var counter = initShow; //counter for load more button
+
+  loadMore(counter, $portfolioContainer)
+  loadMore(counter, $hobbiesContainer)
+
+  function loadMore(toShow, $container) {
+    const iso = $container.data('isotope')
+    $container.find(".hidden").removeClass("hidden");
+
+    var hiddenElems = iso.filteredItems.slice(toShow, iso.filteredItems.length).map(function(item) {
+      return item.element;
+    });
+    $(hiddenElems).addClass('hidden');
+    $container.isotope('layout');
+
+    //when no more to load, hide show more button
+    if (hiddenElems.length == 0) {
+      $container.parent().children(".load-more").hide()
+    } else {
+      $container.parent().children(".load-more").show()
+    };
+
+  }
+
+  $portfolioContainer.after('<a class="button btn btn-primary ease-scroll hover-effect load-more"> Load More</a>');
+  $hobbiesContainer.after('<a class="button btn btn-primary ease-scroll hover-effect load-more"> Load More</a>');
+
+  //when load more button clicked
+  $(".load-more").click(function() {
+    if ($('#filters').data('clicked')) {
+      //when filter button clicked, set initial value for counter
+      counter = initShow;
+      $('#filters').data('clicked', false);
+    } else {
+      counter = counter;
+    };
+
+    counter = counter + initShow;
+  
+    loadMore(counter, $(this).parent().children('.isotope'));
   });
 
+  //when filter button clicked
+  $("#filters").click(function() {
+    $(this).data('clicked', true);
+
+    loadMore(initShow, $(this).parent().parent().children('.isotope'));
+  });
 
   // bind filter button click
-  $('#filters').on('click', 'a', function() {
+  $('#filters a').on('click', function() {
     var filterValue = $(this).attr('data-filter');
+    var $container = $(this).parent().parent().parent().parent().children('.isotope')
+
     $container.isotope({
       filter: filterValue
     });
@@ -61,60 +117,6 @@ $(document).ready(function() {
       $(this).addClass('selected');
     });
   });
-
-  //****************************
-  // Isotope Load more button
-  //****************************
-  var initShow = 6; //number of items loaded on init & onclick load more button
-  var counter = initShow; //counter for load more button
-  var iso = $container.data('isotope'); // get Isotope instance
-
-  console.log(iso)
-
-  loadMore(initShow); //execute function onload
-
-  function loadMore(toShow) {
-    $container.find(".hidden").removeClass("hidden");
-
-    var hiddenElems = iso.filteredItems.slice(toShow, iso.filteredItems.length).map(function(item) {
-      return item.element;
-    });
-    
-    $(hiddenElems).addClass('hidden');
-    $container.isotope('layout');
-
-    //when no more to load, hide show more button
-    if (hiddenElems.length == 0) {
-      jQuery("#load-more").hide();
-    } else {
-      jQuery("#load-more").show();
-    };
-
-  }
-
-  //append load more button
-  $container.after('<a id="load-more" class="button btn btn-primary ease-scroll hover-effect"> Load More</a>');
-
-  //when load more button clicked
-  $("#load-more").click(function() {
-    if ($('#filters').data('clicked')) {
-      //when filter button clicked, set initial value for counter
-      counter = initShow;
-      $('#filters').data('clicked', false);
-    } else {
-      counter = counter;
-    };
-
-    counter = counter + initShow;
-
-    loadMore(counter);
-  });
-
-  //when filter button clicked
-  $("#filters").click(function() {
-    $(this).data('clicked', true);
-    loadMore(initShow);
-  });  
 });
 
 //animation for page elements
@@ -153,69 +155,3 @@ $('.dot').readmore({
 $('.dot').readmore({
   speed: 500
 });
-
-$(document).ready(function () {
-  $("#myButton").click(function () {
-    if ($("link[href*=color]").length) {
-      $("link[href*=color]").remove();
-    } else {
-      var ls = document.createElement('link');
-      ls.rel = "stylesheet";
-      ls.href = "assets/style/css/color.css";
-      document.getElementsByTagName('head')[0].appendChild(ls);
-    }
-  });
-
-});
-
-var initShow = 3; //number of items loaded on init & onclick load more button
-  var counter = initShow; //counter for load more button
-  var iso = $container.data('isotope'); // get Isotope instance
-
-  loadMore(initShow); //execute function onload
-
-  function loadMore(toShow) {
-    $container.find(".hidden").removeClass("hidden");
-
-    var hiddenElems = iso.filteredItems.slice(toShow, iso.filteredItems.length).map(function(item) {
-      return item.element;
-    });
-    $(hiddenElems).addClass('hidden');
-    $container.isotope('layout');
-
-    //when no more to load, hide show more button
-    if (hiddenElems.length == 0) {
-      jQuery("#load-more").hide();
-    } else {
-      jQuery("#load-more").show();
-    };
-
-  }
-
-  //append load more button
-  $container.after('<button id="load-more"> Load More</button>');
-
-  //when load more button clicked
-  $("#load-more").click(function() {
-    if ($('#filters').data('clicked')) {
-      //when filter button clicked, set initial value for counter
-      counter = initShow;
-      $('#filters').data('clicked', false);
-    } else {
-      counter = counter;
-    };
-
-    counter = counter + initShow;
-
-    loadMore(counter);
-  });
-
-  //when filter button clicked
-  $("#filters").click(function() {
-    $(this).data('clicked', true);
-
-    loadMore(initShow);
-  });
-
-  
-  
